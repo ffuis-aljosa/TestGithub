fe008c841cb02b04a6df060933cbd7f08c3e2f84
ffuis-aljosa <Korisnik@FF-206-MAIN-W7>
2015-03-10 08:45:38 +0100

    Initial commit

---
+++ src/testgithub/TestGithub.java
@@ -0,0 +1,8 @@
+package testgithub;
+
+public class TestGithub {
+    public static void main(String[] args) {
+        System.out.println("Hello Github");
+        System.out.println("Hello again");
+    }
+}
