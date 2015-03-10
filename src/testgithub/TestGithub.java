7c58fc81ddab6dd87bf3e51248ede004f4a6310e
Student <Student@Korisnik-PC>
2015-03-10 08:50:08 +0100

    Added another greeting

--- src/testgithub/TestGithub.java
+++ src/testgithub/TestGithub.java
@@ -4,5 +4,6 @@ public class TestGithub {
     public static void main(String[] args) {
         System.out.println("Hello Github");
         System.out.println("Hello again");
+        System.out.println("Hello once again");
     }
 }
